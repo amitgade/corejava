aa46273431c4b04b4375b54bfb295d8b5cec3c15
amitgade <amitgaade@gmail.com>
2020-12-20 21:09:32 +0530

    action-Creaate using FixedThreadPoolExecutor

---
+++ executors/src/common/LoopTaskA.java
@@ -0,0 +1,34 @@
+package common;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by amit on 20-Dec-2020
+ */
+public class LoopTaskA implements Runnable {
+
+    private static int count = 0;
+    private int id = 0;
+
+    public LoopTaskA() {
+        this.id = ++count;
+    }
+
+    @Override
+    public void run() {
+
+        System.out.println("#### <TASK-" + id + "> STARTING ####");
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("<" + id + "> TICK-TICK - " + i);
+            try {
+                TimeUnit.MILLISECONDS.sleep((long) Math.random() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("#### <TASK-" + id + "> COMPLETED ####");
+
+    }
+}
---
+++ executors/src/st01_creating_and_running_executors/UsingFixedThreadPool.java
@@ -0,0 +1,31 @@
+package st01_creating_and_running_executors;
+
+import common.LoopTaskA;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by amit on 20-Dec-2020
+ */
+public class UsingFixedThreadPool {
+    public static void main(String[] args) {
+        System.out.println("Main thread starts");
+
+        // Initialization - Create FixedThreadPool Executor service
+        ExecutorService executorService = Executors.newFixedThreadPool(3); // at the most 3 concurrent threads
+
+        // Service - Submit task(s) to the service using execute()
+        for (int i = 0; i < 6; i++) {
+            executorService.execute(new LoopTaskA());
+        }
+
+        // Destruction - call shutdown on service
+        executorService.shutdown();
+
+        // cannot submit task after service is shutdown - rejected execution exception
+        executorService.execute(new LoopTaskA());
+
+        System.out.println("Main thread ends");
+    }
+}
