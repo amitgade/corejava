a418b3fd86ca0123da633aac261d0bebb9158488
amitgade <amitgaade@gmail.com>
2020-12-20 21:20:35 +0530

    action-Fixed bug in sleep to add random delay

--- executors/src/common/LoopTaskA.java
+++ executors/src/common/LoopTaskA.java
@@ -22,13 +22,12 @@ public class LoopTaskA implements Runnable {
         for (int i = 10; i > 0; i--) {
             System.out.println("<" + id + "> TICK-TICK - " + i);
             try {
-                TimeUnit.MILLISECONDS.sleep((long) Math.random() * 1000);
+                TimeUnit.MILLISECONDS.sleep((long) (Math.random() * 1000));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
         System.out.println("#### <TASK-" + id + "> COMPLETED ####");
-
     }
 }
